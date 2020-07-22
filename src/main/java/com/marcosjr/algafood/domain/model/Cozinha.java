140f5a091f2ee591775b81386ac41dfdb6e7d39f
marcospojr <marcospojr13@gmail.com>
2020-07-22 10:49:02 -0300

    Alterando os nomes das tabelas

--- src/main/java/com/marcosjr/algafood/domain/model/Cozinha.java
+++ src/main/java/com/marcosjr/algafood/domain/model/Cozinha.java
@@ -6,13 +6,11 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "tab_cozinhas")
 public class Cozinha {
 
 	@Id
 	private Long id;
 	
-	@Column(name = "nom_cozinha")
 	private String nome;
 
 	public Long getId() {
